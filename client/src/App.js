7e0e0271dc35fad88c06233eda6cf9a627b0b388
aaron wesolek <maxwes13@gmail.com>
2020-10-11 21:36:15 -0500

    getting started

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ function App() {
       <div className="App">
         <Route exact path="/" component={Login} />
         {/* 
+
+        
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
